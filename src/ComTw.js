ba87b186705c7d0f9b47abf63aeaa79ce8ad06ad
sult <168359467+Salbdr@users.noreply.github.com>
2024-06-09 19:11:04 +0300

    react-lap1

---
+++ src/ComTw.js
@@ -0,0 +1,10 @@
+export default function ComTw() {
+    return(
+        <div>
+            <h1>companyName:twiuaq</h1>
+            <p>worker number:123456</p>
+            <p>year of founded:1234</p>
+            <p>address:123456</p>
+        </div>
+    )
+}
---
+++ src/Employers.js
@@ -0,0 +1,25 @@
+export default function Employers() {
+    return(
+        <div>
+            <h1>Employers</h1>
+            <button>
+                <h2>fahad</h2>
+                <p>languges:js-ts-c#</p>
+                <p>years of work:2</p>
+                <p>company:aramco</p>
+            </button>
+            <button>
+                <h2>fahad</h2>
+                <p>languges:js-ts-c#</p>
+                <p>years of work:2</p>
+                <p>company:aramco</p>
+            </button>
+            <button>
+                <h2>fahad</h2>
+                <p>languges:js-ts-c#</p>
+                <p>years of work:2</p>
+                <p>company:aramco</p>
+            </button>
+        </div>
+    )
+}
